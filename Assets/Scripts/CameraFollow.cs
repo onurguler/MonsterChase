cec6ebe5dae4851ea51eab4c09e171e035b3d027
nattrucci <guleronurosman@gmail.com>
2023-05-25 21:44:47 +0300

    camera following

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Transform player;
+    private Vector3 tempPos;
+
+    [SerializeField]
+    private float minX, maxX;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+    }
+
+    void LateUpdate()
+    {
+        tempPos = transform.position;
+        tempPos.x = player.position.x;
+        
+        if (tempPos.x < minX)
+        {
+            tempPos.x = minX;
+        }
+
+        if (tempPos.x > maxX)
+        {
+            tempPos.x = maxX;
+        }
+
+        transform.position = tempPos;
+    }
+}
