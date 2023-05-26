defac85ac75499323dedace0d7a5a9d8aa51642d
nattrucci <guleronurosman@gmail.com>
2023-05-26 22:32:25 +0300

    Collector hot fix for player

--- Assets/Scripts/Collector.cs
+++ Assets/Scripts/Collector.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class Collector : MonoBehaviour
 {
     private const string ENEMY_TAG = "Enemy";
+    private const string PLAYER_TAG = "Player";
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag(ENEMY_TAG))
+        if (collision.CompareTag(ENEMY_TAG) || collision.CompareTag(PLAYER_TAG))
             Destroy(collision.gameObject);
     }
 }
